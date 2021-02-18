6225f4c1325ec7abd540a2bdd262dc4053bb0036
Shawn Kebel <fxrender@outlook.com>
2021-02-18 10:40:31 -0600

    Work on data and services with API

--- data.ts
+++ data.ts
@@ -1,17 +1,46 @@
+import {IService} from './type';
 import {RiComputerLine} from 'react-icons/ri'
+import {FaServer} from 'react-icons/fa'
+import {AiOutlineAntDesign} from 'react-icons/ai'
+import {AiOutlineApi} from 'react-icons/ai'
+import {MdDeveloperMode} from 'react-icons/md'
 
-interface Service
-
-export const services = [
-
+export const services: IService[] = [
     {
-        title:"Frontend Dev",
-        about:"lorem....",
         Icon: RiComputerLine,
+        title:"Frontend Development",
+        about:
+            "I can build a beautiful and scalable SPA using <b>HTML</b>, <b>CSS</b>, and <b>React.js</b>",
+
     },
     {
-        titlu:123,
-        about:"lorem....",
+        Icon: FaServer,
+        title:"Backend Development",
+        about:
+            "Handle databases, servers, and API's using <b>Express</b> & other popular frameworks",
+    },
+    {
+        Icon: AiOutlineApi,
+        title:"API Development",
+        about:
+            "I can develop robust REST API using <b>Django-rest-api</b> and <b>Node API</b>",
+    },
+    {
+        Icon: MdDeveloperMode,
+        title:"Competitive Coder",
+        about:
+            "A daily problem solver in <b>HackerRank</b> and <b>Leet Code</b>",
+    },
+    {
+        Icon: AiOutlineAntDesign,
+        title:"UI/UX Designer",
+        about:
+            "Stunning user interface designer using <b>Figma</b> and <b>Framer</b>",
+    },
+    {
         Icon: RiComputerLine,
-    }
+        title:"Whatever",
+        about:
+            "drffg erftggybv deessdr sccj ffnngj ffm,ngngjjm jgghhf.",
+    },
 ]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
-const index = () => {
+import {GetServerSidePropsContext, GetStaticPropsContext} from 'next'
+import {services} from '../data'
+
+const index = ({services}) => {
     return (
         <div className="p-4" >
             about page
@@ -7,3 +10,35 @@ const index = () => {
 }
 
 export default index
+
+// export const getServerSideProps = async (
+//     context:getServerSidePropsContext
+// ) => {
+//
+//     // Calculation
+//     const res = await fetch('http://localhost:3000/api/services')
+//     const data = await res.json()
+//
+//     console.log('SERVER', services)
+//
+//     return {
+//         props:{
+//             services: data.services,
+//         },
+//     }
+// }
+//
+// export const getStaticProps = async (context: getStaticPropsContext) => {
+
+    // Calculation
+//     const res = await fetch('http://localhost:3000/api/services')
+//     const data = await res.json()
+//
+//     console.log('SERVER', services)
+//
+//     return {
+//         props:{
+//             services: data.services,
+//         },
+//     }
+// }
